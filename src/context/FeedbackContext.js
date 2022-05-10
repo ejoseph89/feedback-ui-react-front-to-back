a4e7b164d248dccf83bd85c489f87d13e5bbc326
Emil Joseph <emiljoseph89@gmail.com>
2022-05-10 15:54:10 -0600

    bug fix for editing - able to add new item right after editing

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -49,6 +49,11 @@ export const FeedbackProvider = ({children}) => {
     setFeedback(feedback.map((item) => item.id === id ? {
       ...item, ...updatedItem
     } : item))
+
+    setFeedbackEdit({
+      item: {},
+      edit: false
+    })
   }
 
   return (
